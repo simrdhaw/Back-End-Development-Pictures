381c86c70872e55d0c55783349c7c83c4dc9e4c9
simrdhaw <simrandhawan2110@gmail.com>
2023-08-07 18:00:59 -0400

    bug fixes and get pictures api

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,10 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data),200
+    
+    return {"message": "Internal server error"},500
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +47,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    try:
+        return jsonify(data[id-1]),200
+    except:
+        return {"message": "Internal Client Error"},404
 
 
 ######################################################################
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 import json
-import requests
+from flask import request
 
 
 def test_health(client):
